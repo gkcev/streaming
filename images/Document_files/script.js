5be0e878597f7f2c024baafcc9cbd48cf3727611
gkcev <78687136+gkcev@users.noreply.github.com>
2022-11-25 14:42:54 +0300

    Add files via upload

---
+++ images/Document_files/script.js
@@ -0,0 +1,36 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        440:{
+            slidesPerView:1,
+        },
+        680:{
+            slidesPerView:3,
+        },
+        950:{
+            slidesPerView:4,
+        },
+        1180:{
+            slidesPerView:5,
+        },
+        1400:{
+            slidesPerView:6,
+        },
+    }
+  });
